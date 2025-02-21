d16b8d78df5dfcac9f23a9b451929119ee12d97c
vikas <>
2025-02-21 19:01:26 +0530

    getElementById

---
+++ index.js
@@ -0,0 +1,9 @@
+let div = document.getElementById("first");
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px" ;
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
+div.style.color = " blue";
